96287ca1812766707a5099e6038155511a04da2b
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-18 02:41:13 +0530

    Finished the knn algo with the plot as well

--- knn.py
+++ knn.py
@@ -2,6 +2,8 @@ import scipy.io
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
 
 
 data = scipy.io.loadmat('mnist_big.mat')
@@ -13,8 +15,15 @@ X_train = data['X_train']
 Y_train = np.squeeze(data['Y_train'])
 
 k = [1, 5, 10, 15, 20]
-
+accuracy = []
+number = []
 for i in k:
 	neigh = KNeighborsClassifier(n_neighbors=i)
 	neigh.fit(X_train, Y_train)
 	prediction = neigh.predict(X_test)
+	temp = accuracy_score(Y_test, prediction)
+	accuracy.append(temp)
+	number.append(i)
+
+plt.plot(number,accuray,'r-')
+plt.axis([0, 20, 0.5, 1])
