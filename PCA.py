a8b237a596d176441b19afa982f391fca86b5fca
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-18 03:31:47 +0530

    Finished the PCA part of the code now just need to append this to other algos

--- PCA.py
+++ PCA.py
@@ -3,13 +3,25 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
 
 data = scipy.io.loadmat('mnist_big.mat')
 
-X_test = data['X_test']
-Y_test = np.squeeze(data['Y_test'])
+Xtest = data['X_test']
+Ytest = np.squeeze(data['Y_test'])
 
-X_train = data['X_train']
-Y_train = np.squeeze(data['Y_train'])
+Xtrain = data['X_train']
+Ytrain = np.squeeze(data['Y_train'])
+
+dim = [ 50, 100, 200, 300, 500]
+
+for i in dim:
+	pca = PCA(n_components=i)
+	X_train = Xtrain
+	X_test = Xtest 
+	pca.fit(X)
+	pca.transform(temp1)
+
+	
