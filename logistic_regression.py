60cc5b38b89a520d00a8030ff2ee82bd4bcff28e
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-18 04:00:02 +0530

    Started with the logistic regression

---
+++ logistic_regression.py
@@ -0,0 +1,15 @@
+import scipy.io
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+
+
+data = scipy.io.loadmat('mnist_big.mat')
+
+X_test = data['X_test']
+Y_test = np.squeeze(data['Y_test'])
+
+X_train = data['X_train']
+Y_train = np.squeeze(data['Y_train'])
