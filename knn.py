13e03c89d9241a71e7ec1b43d69d6b5db444f0d5
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-18 14:48:36 +0530

    Included a time function to measure the running time of the program

--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+import time
 
 
 
@@ -17,13 +18,19 @@ Y_train = np.squeeze(data['Y_train'])
 k = [1, 5, 10, 15, 20]
 accuracy = []
 number = []
+
+start_time = time.time()
+
 for i in k:
 	neigh = KNeighborsClassifier(n_neighbors=i)
 	neigh.fit(X_train, Y_train)
 	prediction = neigh.predict(X_test)
 	temp = accuracy_score(Y_test, prediction)
+	print classification_report(Y_test, prediction)
 	accuracy.append(temp)
 	number.append(i)
+
+print("The time taken by the knn algorithm is ( %s ) seconds" % (time.time() - start_time))
 
 plt.plot(number,accuracy,'r-')
 plt.axis([0, 20, 0.5, 1])
