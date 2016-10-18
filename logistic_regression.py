66ada1c6a8091d6734eda389bb0d43015cb75cfc
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-18 15:09:06 +0530

    Finished the logistic regression problem

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,9 @@
 import scipy.io
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
 
 
 
@@ -13,3 +14,21 @@ Y_test = np.squeeze(data['Y_test'])
 
 X_train = data['X_train']
 Y_train = np.squeeze(data['Y_train'])
+
+import time
+
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+
+prediction = model.predict(X_test)
+
+temp = accuracy_score(Y_test, prediction)
+
+
+
+print "The accuracy of the logistic regression is %s" %(temp)
+
+print classification_report(Y_test, prediction)
+
+
+print("The time taken by the Logistic Regression algorithm is ( %s ) seconds" % (time.time() - start_time))
