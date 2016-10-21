bf12e6ccb70ab79db72df3fa9e2bb654388daf49
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-21 20:29:00 +0530

    Random forest using cross validation

---
+++ cross_rand_forest.py
@@ -0,0 +1,39 @@
+import scipy.io
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import model_selection
+import matplotlib.pyplot as plt
+import time
+from sklearn.metrics import classification_report
+
+data = scipy.io.loadmat('mnist_big.mat')
+
+X_test = data['X_test']
+Y_test = np.squeeze(data['Y_test'])
+
+X_train = data['X_train']
+Y_train = np.squeeze(data['Y_train'])
+
+trees = [1, 5, 10, 15, 20, 30, 50]
+
+accuracy = []
+number = []
+
+start_time = time.time() 
+
+kfold = model_selection.KFold(n_splits = 10, shuffle = True )
+
+for i in trees:
+	clf = RandomForestClassifier(n_estimators=i)
+	prediction = model_selection.cross_val_score(clf, X_train, Y_train, cv=kfold)
+	temp = sum(prediction)/10;
+	# print classification_report(Y_test, prediction)
+	accuracy.append(temp)
+	number.append(i)
+
+print("The time taken by the knn algorithm is ( %s ) seconds" % (time.time() - start_time))
+
+plt.plot(number,accuracy,'r-')
+plt.axis([0, 50, 0.5, 1])
+plt.show()
