4400fdc681fd1a60bf6039a4f344b2d73921663b
Sai Kishan Pampana <saikishanpampana@gmail.com>
2016-10-24 16:34:23 +0530

    Finished the random forest classifier using cross_validation

--- cross_rand_forest.py
+++ cross_rand_forest.py
@@ -25,15 +25,33 @@ start_time = time.time()
 kfold = model_selection.KFold(n_splits = 10, shuffle = True )
 
 for i in trees:
+	start_new = time.time()
 	clf = RandomForestClassifier(n_estimators=i)
 	prediction = model_selection.cross_val_score(clf, X_train, Y_train, cv=kfold)
 	temp = sum(prediction)/10;
 	# print classification_report(Y_test, prediction)
 	accuracy.append(temp)
 	number.append(i)
+	print("Number of K =  %s" %(i) )
+	print("Accuracy =  %s" %(temp) )
+	print("Time takn = %s" %(time.time() - start_new))
+	print "-----------------------------------"
 
-print("The time taken by the knn algorithm is ( %s ) seconds" % (time.time() - start_time))
+
+print("The time taken by the RandomForestClassifier algorithm is ( %s ) seconds" % (time.time() - start_time))
 
 plt.plot(number,accuracy,'r-')
 plt.axis([0, 50, 0.5, 1])
 plt.show()
+
+best = np.argmax(accuracy)
+
+start = time.time()
+
+clf_best = RandomForestClassifier(n_estimators=30)
+clf_best.fit(X_train, Y_train)
+prediction = clf_best.predict(X_test)
+temp = accuracy_score(Y_test, prediction)
+print("The accuracy on the test data by KNN is %s" %(temp))
+print("The time taken to fit the best tree random forest is (%s) second" %(time.time() - start))
+print classification_report(Y_test, prediction)
